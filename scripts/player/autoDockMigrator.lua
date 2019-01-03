d533b15efa6ae27005e6e1539d64d6ebf3238bbf
Jango Course <jango.course@gmail.com>
2019-01-03 17:23:54 +0100

    Revert "Added"
    This reverts commit f27e76cad1c0d4038b8f0748222105d36efc02f8.

--- scripts/player/autoDockMigrator.lua
+++ scripts/player/autoDockMigrator.lua
@@ -16,18 +16,13 @@ function onSectorChanged()
         for i, station in pairs(stations) do
             local autoDockInstalled = station:getValue("autoDockScriptEnabled")
             if not autoDockInstalled then
-                local unsubscribeAutoDock = station:getValue("noAutoDock")
-                if unsubscribeAutoDock then
-                    print("AutoDock Migrator: Skipping "..tostring(station.translatedTitle).." "..tostring(station.name).."; it has indicated that it does not wish to receive any AutoDocks.");
-                else
-                    print("AutoDock Migrator: Adding AutoDocks to "..tostring(station.translatedTitle).." "..tostring(station.name)..".")
-                    station:addScriptOnce("mods/AutoDock/scripts/entity/autoDockInteraction.lua")
-                    station:setValue("autoDockScriptEnabled", true)
-                    stationsTouched = stationsTouched + 1
+                print("AutoDock Migrator: Adding AutoDocks to "..tostring(station.translatedTitle).." "..tostring(station.name)..".")
+                station:addScriptOnce("mods/AutoDock/scripts/entity/autoDockInteraction.lua")
+                station:setValue("autoDockScriptEnabled", true)
+                stationsTouched = stationsTouched + 1
 
-                    if not touched then
-                        touched = true
-                    end
+                if not touched then
+                    touched = true
                 end
             end
         end
