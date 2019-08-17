5399af758828fab1376eba248d0474f9fd0949e7
Jango Course <jango.course@gmail.com>
2019-08-17 10:36:09 +0200

    Starting to refactor old code. Code isn't in a functional spot right now.

--- data/scripts/entity/autoDockInteraction.lua
+++ data/scripts/entity/autoDockInteraction.lua
@@ -1,7 +1,6 @@
 --Auto-Dock (C) 2018-2019 Shrooblord
 package.path = package.path .. ";data/scripts/lib/?.lua"
 package.path = package.path .. ";data/scripts/entity/?.lua"
-include("stringutility")
 include ("faction")
 include("callable")
 
@@ -12,4 +11,44 @@ AutoDockUI = {}
 local player
 local playerCraft
 local station
+
+local fromScript = "autoDockInteraction"
+
+function AutoDockUI.interactionPossible(playerIndex, option)
+    player = Player(playerIndex)
+    station = Entity()   --Entity() points to the Station this script is running on
+
+    playerCraft = player.craft
+    if playerCraft == nil then return false end
+
+    local playerShip = Entity(playerCraft.index)
+
+    if playerShip then
+        --player is already engaged in an Auto-Docking Sequence
+        if playerShip:getValue("autoDockInProgress") == true then
+            return false
+        end
+        --player is already docked to this Station; no need to initiate Auto-Docking Sequence
+        if station:isDocked(playerShip) then
+            return false
+        end
+    end
+    return true
+end
+
+-- create all required UI elements for the client side
+function AutoDockUI.initUI()
+    ScriptUI():registerInteraction("Auto-Dock to Station"%_t, "onInteract")
+end
+
+function AutoDockUI.onInteract()
+    if onClient() then
+        if playerCraft == nil then return end
+        if station == nil then return end
+
+        invokeServerFunction("resolveInteraction", station.index, player.index)
+
+        ScriptUI():stopInteraction()
+    end
+end
 
